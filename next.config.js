199f98ca070d8b493a13648f2c938f28890801c8
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-15 13:58:54 +0200

    Demo og dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverRuntimeConfig: {},
+    output: 'standalone',
+    reactStrictMode: true,
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+    eslint: {
+        dirs: ['src'],
+        ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
