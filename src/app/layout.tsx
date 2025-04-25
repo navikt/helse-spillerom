9bcfcbc3b200006f26e7c1617e9f6ac0d45dde12
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-25 10:13:18 +0200

    Zod environments og egen tittel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,23 @@ import '../styles/globals.css'
 import type { Metadata } from 'next'
 import React, { PropsWithChildren, ReactElement } from 'react'
 
-import { erLokal } from '@/env'
+import { erDemo, erDev, erLokal } from '@/env'
 import { Header } from '@/components/header/Header'
 import { Preload } from '@/app/preload'
 import { Providers } from '@/app/providers'
 
+function title() {
+    function postfix() {
+        if (erDev) return ' - dev'
+        if (erDemo) return ' - demo'
+        if (erLokal) return ' - localhost'
+        return ''
+    }
+    return `Spillerom${postfix()}`
+}
+
 export const metadata: Metadata = {
-    title: `Speil ${erLokal ? ' - localhost' : ''}`,
+    title: title(),
     icons: {
         icon: `/favicons/${erLokal ? 'favicon-local.ico' : 'favicon-dev.ico'}`,
     },
--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,24 @@
 import process from 'process'
 
+import { z } from 'zod'
+
+export type PublicEnv = z.infer<typeof browserEnvSchema>
+export const browserEnvSchema = z.object({
+    NEXT_PUBLIC_RUNTIME_ENV: z.union([z.literal('dev'), z.literal('lokal'), z.literal('demo'), z.literal('prod')]),
+    NEXT_PUBLIC_ASSET_PREFIX: z.string().optional(),
+})
+
+/**
+ * These envs are available in the browser. They are replaced during the bundling step by NextJS.
+ *
+ * They MUST be provided during the build step.
+ */
+export const browserEnv = browserEnvSchema.parse({
+    NEXT_PUBLIC_RUNTIME_ENV: process.env.NEXT_PUBLIC_RUNTIME_ENV,
+    NEXT_PUBLIC_ASSET_PREFIX: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+} satisfies Record<keyof PublicEnv, string | undefined>)
+
 export const erLokal = process.env.NODE_ENV !== 'production'
+export const erDev = browserEnv.NEXT_PUBLIC_RUNTIME_ENV === 'dev'
+export const erDemo = browserEnv.NEXT_PUBLIC_RUNTIME_ENV === 'demo'
+export const erProd = browserEnv.NEXT_PUBLIC_RUNTIME_ENV === 'prod'
