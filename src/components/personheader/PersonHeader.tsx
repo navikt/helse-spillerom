08db5ac16ca96e4f5612cd42a8067ac39101f359
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-04-25 16:06:43 +0200

    personheader skeleton

--- src/components/personheader/PersonHeader.tsx
+++ src/components/personheader/PersonHeader.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { ReactElement } from 'react'
-import { HStack } from '@navikt/ds-react'
+import { PropsWithChildren, ReactElement } from 'react'
+import { HStack, Skeleton } from '@navikt/ds-react'
 import { FigureCombinationIcon } from '@navikt/aksel-icons'
 
 import { NavnOgAlder } from '@components/personheader/NavnOgAlder'
@@ -12,17 +12,37 @@ import { usePersoninfo } from '@hooks/queries/usePersoninfo'
 export function PersonHeader(): ReactElement {
     const { data: personinfo, isLoading, isError } = usePersoninfo()
 
-    if (isLoading || !personinfo) return <></> // skeleton
-    if (isError) return <></> // feilside
+    if (isLoading) return <PersonHeaderSkeleton />
+    if (isError || !personinfo) return <></> // vis noe fornuftig
 
     return (
-        <HStack as="header" className="h-12 border-b bg-[#f8f8f8] px-8" align="center">
+        <Header>
             <FigureCombinationIcon fontSize="1.5rem" className="mr-2" />
             <NavnOgAlder navn={personinfo.navn} alder={personinfo.alder} />
             <Seperator />
             <Fødselsnummer fødselsnummer={personinfo.fødselsnummer} />
             <Seperator />
             <AktørId aktørId={personinfo.aktørId} />
+        </Header>
+    )
+}
+
+function PersonHeaderSkeleton(): ReactElement {
+    return (
+        <Header>
+            <Skeleton width={210} />
+            <Seperator />
+            <Skeleton width={120} />
+            <Seperator />
+            <Skeleton width={210} />
+        </Header>
+    )
+}
+
+function Header({ children }: PropsWithChildren): ReactElement {
+    return (
+        <HStack as="header" className="h-12 border-b bg-[#f8f8f8] px-8" align="center">
+            {children}
         </HStack>
     )
 }
