181a3a784985794c781181e7509528f80560969e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-28 14:35:24 +0200

    Api proxy mot bakrommet

--- src/app/api/bakrommet/v1/personsok/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-import { ErrorResponse } from '@/auth/beskyttetApi'
-import { PersonId } from '@/schemas/personsøk'
-
-export async function POST(): Promise<NextResponse<PersonId | ErrorResponse>> {
-    return NextResponse.json({ personId: '1234567891011' })
-}
