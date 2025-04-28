cc391fa4283e31a2f358d3429b872e4a6c4646b2
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-04-28 11:44:36 +0200

    tar i bruk fetchAndParse util i useBrukerinfo

--- src/hooks/queries/useBrukerinfo.ts
+++ src/hooks/queries/useBrukerinfo.ts
@@ -1,20 +1,11 @@
-'use client'
-
 import { useQuery } from '@tanstack/react-query'
 
 import { Bruker, brukerSchema } from '@/schemas/bruker'
+import { fetchAndParse } from '@utils/fetch'
 
 export function useBrukerinfo() {
     return useQuery<Bruker, Error>({
         queryKey: ['personinfo'],
-        queryFn: async () => {
-            const json = await (
-                await fetch(`/api/v1/bruker`, {
-                    method: 'GET',
-                })
-            ).json()
-
-            return brukerSchema.parse(json)
-        },
+        queryFn: async () => fetchAndParse('/api/v1/bruker', brukerSchema),
     })
 }
