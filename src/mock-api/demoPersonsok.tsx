a5a7aae4d0885f39ef5fe49f45b8afcc82b91d3a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-29 14:18:37 +0200

    tmp

--- src/mock-api/demoPersonsok.tsx
+++ src/mock-api/demoPersonsok.tsx
@@ -1,25 +1,47 @@
 'use client'
 
-import { erLokalEllerDemo } from '@/env'
+import {erLokalEllerDemo} from '@/env'
+import {Button, Heading, Modal, Tooltip} from "@navikt/ds-react";
+import React, {useState} from "react";
+import {SandboxIcon} from "@navikt/aksel-icons";
 
 type LayoutWrapperProps = {
     children: React.ReactNode
 }
 
-export const DemoPersonsok: React.FC<LayoutWrapperProps> = ({ children }) => {
+export const DemoPersonsok: React.FC<LayoutWrapperProps> = ({children}) => {
+    const [openState, setOpenState] = useState(false)
+
     if (!erLokalEllerDemo) {
         return <>{children}</>
     }
     return (
         <div className="relative min-h-screen">
             {children}
-            <button
-                onClick={() => window.alert('personvalg')}
-                className="fixed right-4 bottom-4 z-50 rounded-full bg-blue-600 p-4 text-white shadow-lg transition-all hover:bg-blue-700"
-                aria-label="Floating action button"
+
+            <div className={"fixed right-4 bottom-4 z-50"}>
+
+            <Tooltip  content="Testpersoner">
+                <Button
+                    type="button"
+                    onClick={() => setOpenState((b) => !b)}
+                    icon={<SandboxIcon title="Åpne testdataverktøy" aria-hidden/>}
+                    variant="tertiary-neutral"
+                />
+            </Tooltip>
+            </div>
+            <Modal
+                open={openState}
+                onClose={() => {
+                    setOpenState(false)
+                }}
+                header={{heading: 'Testdataverktøy', closeButton: true}}
+                className="h-screen max-h-max max-w-[369px] rounded-none p-0 left-auto m-0"
             >
-                dsaf
-            </button>
+                <Modal.Body>
+                    easfd
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
