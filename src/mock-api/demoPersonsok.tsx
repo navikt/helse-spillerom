954b597e3f00654cb547b27c2b1b81ebd0420d4c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-04-29 13:59:52 +0200

    Fjern consol log

--- src/mock-api/demoPersonsok.tsx
+++ src/mock-api/demoPersonsok.tsx
@@ -1,22 +1,20 @@
-"use client"
+'use client'
 
-import {erLokalEllerDemo} from "@/env";
+import { erLokalEllerDemo } from '@/env'
 
 type LayoutWrapperProps = {
     children: React.ReactNode
 }
 
 export const DemoPersonsok: React.FC<LayoutWrapperProps> = ({ children }) => {
-    console.log(erLokalEllerDemo)
-
-    if(!erLokalEllerDemo){
+    if (!erLokalEllerDemo) {
         return <>{children}</>
     }
     return (
         <div className="relative min-h-screen">
             {children}
             <button
-                onClick={() => console.log('sdf')}
+                onClick={() => window.alert('personvalg')}
                 className="fixed right-4 bottom-4 z-50 rounded-full bg-blue-600 p-4 text-white shadow-lg transition-all hover:bg-blue-700"
                 aria-label="Floating action button"
             >
