4923443f9824226f2ba266f3d82033469346c5a6
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-05-09 08:11:22 +0200

    Legger til Inngangskategori

--- src/components/saksbilde/start-behandling/StartBehandling.tsx
+++ src/components/saksbilde/start-behandling/StartBehandling.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ReactElement } from 'react'
-import { Checkbox, CheckboxGroup, Label } from '@navikt/ds-react'
+import { Button, Checkbox, CheckboxGroup, Label, Select } from '@navikt/ds-react'
 
 import { SaksbildePanel } from '@components/saksbilde/SaksbildePanel'
 import { useSoknader } from '@hooks/queries/useSoknader'
@@ -25,7 +25,7 @@ export function StartBehandling({ value }: StartBehandlingProps): ReactElement {
         acc[key].push(soknad)
         return acc
     }, {})
-
+    //TODO form
     return (
         <SaksbildePanel value={value}>
             <Label spacing>Velg hvilken søknad som skal behandles</Label>
@@ -40,6 +40,37 @@ export function StartBehandling({ value }: StartBehandlingProps): ReactElement {
                     </CheckboxGroup>
                 </div>
             ))}
+            <Select label="Hvilken inntektskategori tilhører søkeren?" className="my-8">
+                {arbeidssituasjoner.map((situasjon) => (
+                    <option key={situasjon} value={situasjon}>
+                        {situasjon}
+                    </option>
+                ))}
+            </Select>
+            <Button size="small">Start behandling</Button>
         </SaksbildePanel>
     )
 }
+
+const arbeidssituasjoner = [
+    'ARBEIDSTAKER',
+    'SELVSTENDIG NÆRINGSDRIVENDE',
+    'SJØMANN',
+    'JORDBRUKER',
+    'ARBEIDSLEDIG',
+    'INAKTIV',
+    'MILITÆR (BEFAL)',
+    'VERNEPLIKTIG',
+    'ARBEIDSTAKER M/SJØMANN',
+    'INAKTIVT M/SJØMANN',
+    'SVALBARDARBEIDER',
+    'AMBASSADEPERSONELL',
+    'UTØVER AV REINDRIFT',
+    'FISKE/ARBEIDSTAKER',
+    'FRILANSER M/FORSIKRING FOR TILLEGGSSYKEPENGER',
+    'FFU',
+    'ARBEIDSTAKER / A-LØYSE',
+    'FRILANSER UTEN FORSIKRING',
+    'SELVSTENDIG DAGMAMMA/DAGPAPPA',
+    'FISKER M/HYRE',
+]
