f96e09b82225e8d6190e3483bbd6cd447dd92c63
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-05-09 08:07:38 +0200

    Lister opp søknader i start behandling

--- src/components/saksbilde/start-behandling/StartBehandling.tsx
+++ src/components/saksbilde/start-behandling/StartBehandling.tsx
@@ -1,11 +1,45 @@
+'use client'
+
 import { ReactElement } from 'react'
+import { Checkbox, CheckboxGroup, Label } from '@navikt/ds-react'
 
 import { SaksbildePanel } from '@components/saksbilde/SaksbildePanel'
+import { useSoknader } from '@hooks/queries/useSoknader'
+import { Søknad } from '@/schemas/søknad'
+import { getFormattedDateString } from '@utils/date-format'
 
 interface StartBehandlingProps {
     value: string
 }
 
 export function StartBehandling({ value }: StartBehandlingProps): ReactElement {
-    return <SaksbildePanel value={value}>Start behandling</SaksbildePanel>
+    const { data: søknader, isLoading, isError } = useSoknader()
+
+    if (isLoading) return <></>
+    if (isError || !søknader) return <></> // vis noe fornuftig
+
+    const søknaderGruppert = søknader.reduce((acc: Record<string, Søknad[]>, soknad) => {
+        const key = soknad.arbeidsgiver?.navn || soknad.arbeidssituasjon || soknad.type
+
+        acc[key] = acc[key] || []
+        acc[key].push(soknad)
+        return acc
+    }, {})
+
+    return (
+        <SaksbildePanel value={value}>
+            <Label spacing>Velg hvilken søknad som skal behandles</Label>
+            {Object.entries(søknaderGruppert).map(([key, gruppe]) => (
+                <div key={key} className="mt-4">
+                    <CheckboxGroup legend={key}>
+                        {gruppe.map((søknad, j) => (
+                            <Checkbox key={j}>
+                                {getFormattedDateString(søknad.fom) + ' - ' + getFormattedDateString(søknad.tom)}
+                            </Checkbox>
+                        ))}
+                    </CheckboxGroup>
+                </div>
+            ))}
+        </SaksbildePanel>
+    )
 }
