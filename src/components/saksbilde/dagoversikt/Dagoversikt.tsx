79ef181fa6fd61e535bed9063c78fa9b1546f975
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-06-03 09:02:32 +0200

    Fikser bygget

--- src/components/saksbilde/dagoversikt/Dagoversikt.tsx
+++ src/components/saksbilde/dagoversikt/Dagoversikt.tsx
@@ -1,16 +1,11 @@
 import { ReactElement } from 'react'
 
 import { SaksbildePanel } from '@components/saksbilde/SaksbildePanel'
-import { VilkårsvurderingDebug } from '@components/saksbilde/vilkårsvurdering/VilkårsvurderingDebug'
 
 interface DagoversiktProps {
     value: string
 }
 
 export function Dagoversikt({ value }: DagoversiktProps): ReactElement {
-    return (
-        <SaksbildePanel value={value}>
-            <VilkårsvurderingDebug />
-        </SaksbildePanel>
-    )
+    return <SaksbildePanel value={value}>Dagoversikt</SaksbildePanel>
 }
