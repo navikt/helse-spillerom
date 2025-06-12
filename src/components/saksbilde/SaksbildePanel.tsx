9626f9250ace8e1769439b0ff1eb4a67ef3a1776
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-06-12 13:11:55 +0200

    mini refaktorering

--- src/components/saksbilde/SaksbildePanel.tsx
+++ src/components/saksbilde/SaksbildePanel.tsx
@@ -1,11 +1,7 @@
 import { PropsWithChildren, ReactElement } from 'react'
 import { TabsPanel } from '@navikt/ds-react/Tabs'
 
-export interface SaksbildePanelProps extends PropsWithChildren {
-    value: string
-}
-
-export function SaksbildePanel({ value, children }: SaksbildePanelProps): ReactElement {
+export function SaksbildePanel({ value, children }: PropsWithChildren<{ value: string }>): ReactElement {
     return (
         <TabsPanel value={value} className="p-8">
             {children}
