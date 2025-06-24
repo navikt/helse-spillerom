049086e6f64c7fe4d2ec180a0e2fa8660c8d62fc
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-06-24 14:17:26 +0200

    🚧 Legg til knapper for individuell begrunnelse og godkjenning

--- src/components/sidemenyer/venstremeny/Venstremeny.tsx
+++ src/components/sidemenyer/venstremeny/Venstremeny.tsx
@@ -1,13 +1,65 @@
+'use client'
+
 import { ReactElement } from 'react'
+import { useParams } from 'next/navigation'
+import { Button, VStack, BodyShort, HStack } from '@navikt/ds-react'
+import { CalendarIcon, PencilIcon } from '@navikt/aksel-icons'
 
 import { Sidemeny } from '@components/sidemenyer/Sidemeny'
+import { useSaksbehandlingsperioder } from '@hooks/queries/useSaksbehandlingsperioder'
+import { getFormattedDateString } from '@utils/date-format'
 
 import { KategoriTag } from './KategoriTag'
 
 export function Venstremeny(): ReactElement {
+    const params = useParams()
+    const { data: saksbehandlingsperioder } = useSaksbehandlingsperioder()
+
+    // Finn aktiv saksbehandlingsperiode hvis vi er inne i en
+    const aktivSaksbehandlingsperiode = saksbehandlingsperioder?.find(
+        (periode) => periode.id === params.saksbehandlingsperiodeId,
+    )
+
     return (
         <Sidemeny side="left">
-            <KategoriTag />
+            <VStack gap="4">
+                <KategoriTag />
+
+                {aktivSaksbehandlingsperiode && (
+                    <>
+                        <HStack gap="2" align="center">
+                            <CalendarIcon aria-hidden fontSize="1.25rem" />
+                            <BodyShort size="small">
+                                {getFormattedDateString(aktivSaksbehandlingsperiode.fom)} -{' '}
+                                {getFormattedDateString(aktivSaksbehandlingsperiode.tom)}
+                            </BodyShort>
+                        </HStack>
+
+                        <Button
+                            variant="tertiary"
+                            size="small"
+                            icon={<PencilIcon aria-hidden fontSize="1.25rem" />}
+                            className="w-fit"
+                            onClick={() => {
+                                // TODO: Implementer individuell begrunnelse funksjonalitet
+                            }}
+                        >
+                            Skriv individuell begrunnelse
+                        </Button>
+
+                        <Button
+                            variant="primary"
+                            size="small"
+                            className="w-fit"
+                            onClick={() => {
+                                // TODO: Implementer send til godkjenning funksjonalitet
+                            }}
+                        >
+                            Send til godkjenning
+                        </Button>
+                    </>
+                )}
+            </VStack>
         </Sidemeny>
     )
 }
