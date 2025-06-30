aa535f3946ee30d0b40429768867844a91e1562c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-06-30 12:43:32 +0200

    Flere kategorier

--- src/components/saksbilde/vilkårsvurdering/Vilkårsvurdering.tsx
+++ src/components/saksbilde/vilkårsvurdering/Vilkårsvurdering.tsx
@@ -179,4 +179,10 @@ const kategoriTekst: Record<string, string> = {
     generelle_bestemmelser: 'Generelle bestemmelser',
     arbeidstakere: 'Arbeidstakere',
     selvstendig_næringsdrivende: 'Selvstendige næringsdrivende',
+    frilansere: 'Frilansere',
+    medlemmer_med_kombinerte_inntekter: 'Medlemmer med kombinerte inntekter',
+    særskilte_grupper: 'Særskilte grupper',
+    medlemmer_med_rett_til_andre_ytelser: 'Medlemmer som har rett til andre ytelser fra folketrygden',
+    opphold_i_institusjon: 'Opphold i institusjon',
+    yrkesskade: 'Yrkesskade',
 }
