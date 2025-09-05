880eb7eacc699e510f078c0597904d0ca49e03c1
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-09-05 17:50:20 +0200

    ♻️ fikser navn på props i FetchError

--- src/components/saksbilde/FetchError.tsx
+++ src/components/saksbilde/FetchError.tsx
@@ -1,12 +1,12 @@
 import { ReactElement } from 'react'
 import { Alert, BodyShort, Button, HStack } from '@navikt/ds-react'
 
-interface ErrorWithRefetchProps {
+interface FetchErrorProps {
     refetch: () => void
     message: string
 }
 
-export function FetchError({ refetch, message }: ErrorWithRefetchProps): ReactElement {
+export function FetchError({ refetch, message }: FetchErrorProps): ReactElement {
     return (
         <Alert variant="error">
             <HStack gap="4">
