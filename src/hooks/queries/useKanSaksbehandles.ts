ab805cd333ef25c4d32eeca5027dcab14cceb352
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-09-12 15:03:19 +0200

    Saksbehandleren må eie saken

--- src/hooks/queries/useKanSaksbehandles.ts
+++ src/hooks/queries/useKanSaksbehandles.ts
@@ -2,14 +2,26 @@ import { useMemo } from 'react'
 
 import { useBrukerRoller } from './useBrukerRoller'
 import { useAktivSaksbehandlingsperiode } from './useAktivSaksbehandlingsperiode'
+import { useBrukerinfo } from './useBrukerinfo'
 
 export function useKanSaksbehandles() {
     const { data: brukerRoller } = useBrukerRoller()
+    const { data: brukerinfo } = useBrukerinfo()
+
     const aktivSaksbehandlingsperiode = useAktivSaksbehandlingsperiode()
 
     const kanSaksbehandles = useMemo(() => {
-        return brukerRoller.saksbehandler && aktivSaksbehandlingsperiode?.status === 'UNDER_BEHANDLING'
-    }, [brukerRoller.saksbehandler, aktivSaksbehandlingsperiode?.status])
+        return (
+            brukerRoller.saksbehandler &&
+            aktivSaksbehandlingsperiode?.status === 'UNDER_BEHANDLING' &&
+            aktivSaksbehandlingsperiode.opprettetAvNavIdent === brukerinfo?.navIdent
+        )
+    }, [
+        brukerRoller.saksbehandler,
+        aktivSaksbehandlingsperiode?.status,
+        aktivSaksbehandlingsperiode?.opprettetAvNavIdent,
+        brukerinfo?.navIdent,
+    ])
 
     return kanSaksbehandles
 }
