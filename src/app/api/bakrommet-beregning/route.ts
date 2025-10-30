cb880e6750019556bcd9715a469e660e75dfd48e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-10-30 09:35:51 +0100

    Bruker vanlig fetch mot bakrommet beregning

--- src/app/api/bakrommet-beregning/route.ts
+++ src/app/api/bakrommet-beregning/route.ts
@@ -1,19 +1,41 @@
 import { logger } from '@navikt/next-logger'
-import { proxyRouteHandler } from '@navikt/next-api-proxy'
 
-import { erDemo, getServerEnv } from '@/env'
+import { erDemo } from '@/env'
 
-export async function POST(request: Request): Promise<Response> {
+export async function POST(req: Request): Promise<Response> {
     // Kun tillatt i demo-versjonen
     if (!erDemo) {
         logger.warn('Bakrommet beregning API kun tillatt i demo-versjonen')
         return Response.json({ message: 'API kun tilgjengelig i demo-versjonen' }, { status: 403 })
     }
-    const serverEnv = getServerEnv()
 
-    return await proxyRouteHandler(request, {
-        hostname: serverEnv.BAKROMMET_HOST,
-        path: '/api/demo/utbetalingsberegning',
-        https: false,
-    })
+    try {
+        logger.info('Proxying beregning request til bakrommet')
+
+        // Proxy requesten til bakrommet
+        const response = await fetch('http://bakrommet/api/demo/utbetalingsberegning', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: await req.text(),
+        })
+
+        const data = await response.text()
+
+        // Returner responsen fra bakrommet med headers og statuskode
+        const headers = new Headers()
+        response.headers.forEach((value, key) => {
+            headers.set(key, value)
+        })
+
+        return new Response(data, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: headers,
+        })
+    } catch (error) {
+        logger.error(`Feil ved kall til bakrommet API i beregning route: ${JSON.stringify(error)}`)
+        return Response.json({ message: 'Feil ved kall til bakrommet API' }, { status: 500 })
+    }
 }
