07449bc40a3084c08172b0c1bb5ffa043d8baf41
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-11-20 11:45:23 +0100

    💄 Endrer variant på knapp i åpent dokument

--- src/components/sidemenyer/høyremeny/dokumenter/InntektsmeldingInnhold.tsx
+++ src/components/sidemenyer/høyremeny/dokumenter/InntektsmeldingInnhold.tsx
@@ -40,7 +40,7 @@ export function InntektsmeldingInnhold({
             <Details label="Innsender telefon">{inntektsmelding.innsenderTelefon}</Details>
             <Details label="Avsendersystem">{inntektsmelding.avsenderSystem?.navn}</Details>
             {handleSelect && (
-                <Button type="button" size="small" onClick={handleSelect}>
+                <Button type="button" variant="secondary" size="small" onClick={handleSelect}>
                     Velg
                 </Button>
             )}
