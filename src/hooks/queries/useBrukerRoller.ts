ef52502f752fd795c840262c6f6130e9dd57affa
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-12-03 19:26:15 +0100

    ♻️ Fjerner useMemo fra useBrukerRoller
    React Compiler fikser om nødvendig

--- src/hooks/queries/useBrukerRoller.ts
+++ src/hooks/queries/useBrukerRoller.ts
@@ -1,26 +1,13 @@
-import { useMemo } from 'react'
-
 import { useBrukerinfo } from '@hooks/queries/useBrukerinfo'
-
-type BrukerRoller = {
-    leserolle: boolean
-    saksbehandler: boolean
-    beslutter: boolean
-}
 
 export function useBrukerRoller() {
     const { data: brukerinfo } = useBrukerinfo()
 
-    const data: BrukerRoller = useMemo(
-        () => ({
+    return {
+        data: {
             leserolle: brukerinfo?.roller?.includes('LES') ?? false,
             saksbehandler: brukerinfo?.roller?.includes('SAKSBEHANDLER') ?? false,
             beslutter: brukerinfo?.roller?.includes('BESLUTTER') ?? false,
-        }),
-        [brukerinfo?.roller],
-    )
-
-    return {
-        data,
+        },
     }
 }
