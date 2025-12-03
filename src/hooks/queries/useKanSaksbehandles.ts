1ac58f65d3e8c9cf66af9daabedafa0df87529b0
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-12-03 18:51:40 +0100

    ♻️ Fjerner useMemo fra useKanSaksbehandles
    Lar React Compiler fikse om nødvendig

--- src/hooks/queries/useKanSaksbehandles.ts
+++ src/hooks/queries/useKanSaksbehandles.ts
@@ -1,5 +1,3 @@
-import { useMemo } from 'react'
-
 import { useBrukerRoller } from './useBrukerRoller'
 import { useAktivSaksbehandlingsperiode } from './useAktivSaksbehandlingsperiode'
 import { useBrukerinfo } from './useBrukerinfo'
@@ -7,21 +5,11 @@ import { useBrukerinfo } from './useBrukerinfo'
 export function useKanSaksbehandles() {
     const { data: brukerRoller } = useBrukerRoller()
     const { data: brukerinfo } = useBrukerinfo()
-
     const aktivSaksbehandlingsperiode = useAktivSaksbehandlingsperiode()
 
-    const kanSaksbehandles = useMemo(() => {
-        return (
-            brukerRoller.saksbehandler &&
-            aktivSaksbehandlingsperiode?.status === 'UNDER_BEHANDLING' &&
-            aktivSaksbehandlingsperiode.opprettetAvNavIdent === brukerinfo?.navIdent
-        )
-    }, [
-        brukerRoller.saksbehandler,
-        aktivSaksbehandlingsperiode?.status,
-        aktivSaksbehandlingsperiode?.opprettetAvNavIdent,
-        brukerinfo?.navIdent,
-    ])
-
-    return kanSaksbehandles
+    return (
+        brukerRoller.saksbehandler &&
+        aktivSaksbehandlingsperiode?.status === 'UNDER_BEHANDLING' &&
+        aktivSaksbehandlingsperiode.opprettetAvNavIdent === brukerinfo?.navIdent
+    )
 }
