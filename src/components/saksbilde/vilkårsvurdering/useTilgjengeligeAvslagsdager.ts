6f6f916a431989789fc98be792c81c0e8e71c177
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-12-03 18:44:52 +0100

    ♻️ Fjerner useMemo fra useTilgjengeligeAvslagsdager
    Lar React Compiler fikse om nødvendig

--- src/components/saksbilde/vilkårsvurdering/useTilgjengeligeAvslagsdager.ts
+++ src/components/saksbilde/vilkårsvurdering/useTilgjengeligeAvslagsdager.ts
@@ -1,5 +1,3 @@
-import { useMemo } from 'react'
-
 import { useKodeverk } from '@/hooks/queries/useKodeverk'
 import { useVilkaarsvurderinger } from '@/hooks/queries/useVilkaarsvurderinger'
 
@@ -16,28 +14,26 @@ export function useTilgjengeligeAvslagsdager(): Avslagsdag[] {
     const { data: kodeverk = [] } = useKodeverk()
     const { data: vilkårsvurderinger = [] } = useVilkaarsvurderinger()
 
-    return useMemo(() => {
-        // Samle alle årsakskoder som er valgt i vilkårsvurderinger med IKKE_OPPFYLT
-        const avslagsdagerMap = new Map<string, Avslagsdag>()
+    // Samle alle årsakskoder som er valgt i vilkårsvurderinger med IKKE_OPPFYLT
+    const avslagsdagerMap = new Map<string, Avslagsdag>()
 
-        vilkårsvurderinger.forEach((vurdering) => {
-            if (vurdering.vurdering === 'IKKE_OPPFYLT') {
-                vurdering.underspørsmål.forEach((underspørsmål) => {
-                    // Finn årsaken i kodeverket for å få beskrivelse
-                    for (const vilkår of kodeverk) {
-                        const årsak = vilkår.ikkeOppfylt.find((årsak) => årsak.kode === underspørsmål.svar)
-                        if (årsak) {
-                            avslagsdagerMap.set(årsak.kode, {
-                                kode: årsak.kode,
-                                beskrivelse: årsak.beskrivelse,
-                            })
-                            break
-                        }
+    vilkårsvurderinger.forEach((vurdering) => {
+        if (vurdering.vurdering === 'IKKE_OPPFYLT') {
+            vurdering.underspørsmål.forEach((underspørsmål) => {
+                // Finn årsaken i kodeverket for å få beskrivelse
+                for (const vilkår of kodeverk) {
+                    const årsak = vilkår.ikkeOppfylt.find((årsak) => årsak.kode === underspørsmål.svar)
+                    if (årsak) {
+                        avslagsdagerMap.set(årsak.kode, {
+                            kode: årsak.kode,
+                            beskrivelse: årsak.beskrivelse,
+                        })
+                        break
                     }
-                })
-            }
-        })
+                }
+            })
+        }
+    })
 
-        return Array.from(avslagsdagerMap.values()).sort((a, b) => a.kode.localeCompare(b.kode))
-    }, [kodeverk, vilkårsvurderinger])
+    return Array.from(avslagsdagerMap.values()).sort((a, b) => a.kode.localeCompare(b.kode))
 }
