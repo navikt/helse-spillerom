a05432c044bddf7c6c99f6e12a9f496242124349
Elias Andreassen Thøgersen <elias3004@msn.com>
2025-12-03 14:00:05 +0100

    ➕ legger til babel-plugin-react-compiler og skrur på React Compiler

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     output: 'standalone',
     reactStrictMode: true,
+    reactCompiler: true,
     assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
     serverExternalPackages: ['@navikt/next-logger'],
     experimental: {
