ec2c0bbad8851b29d0702e0385116b5f70d528d3
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-12-15 15:16:58 +0100

    Fjerner hardkodet testdata

--- src/hooks/queries/useValideringer.ts
+++ src/hooks/queries/useValideringer.ts
@@ -11,25 +11,11 @@ export function useValideringer() {
 
     return useQuery<Validering[], Error>({
         queryKey: queryKeys.valideringer(pseudoId, behandlingId),
-        queryFn: () => {
-            if (1 < 2) {
-                return [
-                    {
-                        id: '123e4567-e89b-12d3-a456-426614174000',
-                        tekst: 'Dette er en valideringsmelding.',
-                    },
-                    {
-                        id: '123e4567-e89b-12d3-a456-426614174001',
-                        tekst: 'Dette er enda en valideringsmelding.',
-                    },
-                ]
-            }
-
-            return fetchAndParse(
+        queryFn: () =>
+            fetchAndParse(
                 `/api/bakrommet/v1/${pseudoId}/behandlinger/${behandlingId}/validering`,
                 z.array(valideringSchema),
-            )
-        },
+            ),
         enabled: Boolean(pseudoId && behandlingId),
     })
 }
